e9fad199cb2fd8bd05e3aaf59ceb9fa9c4c2888e
Otter <joe_sep98@hotmail.com>
2021-05-02 15:52:50 -0400

    db

--- commands/ff.js
+++ commands/ff.js
@@ -1,19 +1,35 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('ff');
 
 module.exports.run = async (bot, msg, args) => {
-    db.serialize(function() {
-        db.run("CREATE TABLE [IF NOT EXISTS] users "
-              + "userID INTEGER PRIMARY KEY,"
-              + "status INTEGER,"
-              + "blockID INTEGER");
-        
-        //var stmt = db.
-      });
-      
-      db.close();
+    var db = new sqlite3.Database('ff.db');
+
+    db.run("CREATE TABLE IF NOT EXISTS users("
+            + "userID INTEGER PRIMARY KEY,"
+            + "status INTEGER,"
+            + "blockID INTEGER);");
+            
+    var userID = 2;
+    var status = 2;
+    var blockID = 3;
+    db.run(`INSERT INTO users VALUES(${userID}, ${status}, ${blockID})`, function(err){
+        if (err){
+            return console.log(err.message);
+        }
+    });
+    
+    db.all("Select * from users",[], (err, rows) => {
+        if (err){
+            throw err
+        }
+
+        rows.forEach((row) => {
+            console.log(row)
+        })
+    });
+
+    db.close();
 }
 
 module.exports.help = {

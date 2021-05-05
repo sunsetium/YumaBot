ce8c081b43c3375b24fc8854e18448a65b3bd4e1
sunsetium <patelsunny@live.ca>
2021-05-05 15:14:06 -0400

    role reactions are now working

--- commands/setup.js
+++ commands/setup.js
@@ -1,7 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+const { userInfo } = require("os");
 var sqlite3 = require('sqlite3').verbose();
+
+
+const roleName = 'Looking for Friend';
+const botStartMsg = "Friendship ;-; plz";
+botStartReaction = ['😄'];
+var roleID;
+var specificMsgID;
+var specificChannelID;
+emoteName = '😄';
 
  /*
   Add this commented section to the command that would create the new channel.
@@ -21,7 +31,6 @@ const addReactions = (message, reactions) => {
 
   module.exports.run = async (bot, msg, args) => {
 
-
     // Create a new text channel
     const createdCh = msg.guild.channels.create('friend-finding')
       .then((ch) => {msg.reply(`Friend finding channel has now been created ${ch}`);
@@ -30,26 +39,83 @@ const addReactions = (message, reactions) => {
     .then((channelID) => {
       const specificChannel = msg.guild.channels.cache.find(channel => channel.id === channelID.id);
       //specificChannel.send("React for friendship ;-;");
-      return specificChannel
+      specificChannelID = specificChannel.id;
+      return specificChannel;
     })
     .then((specificChannel) =>{
 
-      text = "Friendship ;-; plz";
-      reactions = ['😄'];
-
 //review bottom portions to always listen to reactions.
-      specificChannel.messages.fetch().then((messages) => {
+      var specificMsg = specificChannel.messages.fetch().then((messages) => {
         if (messages.size === 0) {
           // Send a new message
-          specificChannel.send(text).then((message) => {
-            addReactions(message, reactions)
+          specificChannel.send(botStartMsg).then((message) => {
+            addReactions(message, botStartReaction)
           })
         } else {
           // Edit the existing message
           for (const message of messages) {
-            message[1].edit(text)
-            addReactions(message[1], reactions)
+            message[1].edit(botStartMsg)
+            addReactions(message[1], botStartReaction)
           }
+        }
+      })
+      specificMsgID = specificMsg.id;
+      console.log(specificMsg);
+
+      return specificMsg;
+      //return specificChannel;
+    })
+    .then((specificMsg) => {
+        //role created.
+        var roleCreated = msg.guild.roles.create({
+          data: {
+            name : roleName,
+          },
+          reason: 'We need a role for those that want to participate.'
+        })
+        roleID = roleCreated.id;
+        return roleCreated;
+    }).then((roleCreated) => {
+      const handleReaction = (reaction, user, add) =>{
+        if(user.id === '670666579167412225'){
+          return;
+        }
+        const emoji = reaction._emoji.name;
+  
+        const { guild } = reaction.message
+  
+        const role = guild.roles.cache.find(role => role.id === roleCreated.id)
+        const member = guild.members.cache.find(member => member.id === user.id);
+  
+        if(role == null)
+        {
+          console.log('role does not exist');
+        }
+        else{
+          console.log(role);
+        }
+  
+        if(add){
+          member.roles.add(role);
+        }
+        else{
+          member.roles.remove(role);
+        }
+  
+      }
+  
+      bot.on('messageReactionAdd', (reaction, user) => {
+        if(specificChannelID === reaction.message.channel.id)
+        {
+          handleReaction(reaction,user, true);
+        }
+  
+      })
+  
+      bot.on('messageReactionRemove', (reaction, user) => {
+        if(specificChannelID === reaction.message.channel.id)
+        {
+          handleReaction(reaction,user, false);
         }
       })
       
